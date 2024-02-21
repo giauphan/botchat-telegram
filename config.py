d3864360c99453e074e1fc095afbd05d14265f36
giauphan <giauphan012@gmail.com>
2024-02-21 17:03:01 +0700

    update code

---
+++ config.py
@@ -0,0 +1,7 @@
+import os
+import sys
+
+def add_parent_to_path():
+    CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
+    PARENT_DIR = os.path.dirname(CURRENT_DIR)
+    sys.path.append(PARENT_DIR)
--- migrations/ChatSeed.py
+++ migrations/ChatSeed.py
@@ -2,6 +2,7 @@ import orm
 import os, sys
 CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(os.path.dirname(CURRENT_DIR))
+
 from Model.Chat import ChatModel
 
 async def main():
