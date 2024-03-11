643015e114b4d2a82efcce9b89e0cd8f5efd0044
giauphan <giauphan012@gmail.com>
2024-03-11 16:45:15 +0700

    Done feat send email notification

--- app/feat/sendEmail.py
+++ app/feat/sendEmail.py
@@ -6,25 +6,33 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+def connect_to_smtp():
+    sender_email = os.getenv('SENDER_EMAIL')
+    smtp_server = os.getenv('SMTP_SERVER')
+    port = os.getenv('SMTP_PORT')
+    password = os.getenv('SMTP_PASSWORD')
+
+    server = smtplib.SMTP(smtp_server, port)
+    server.starttls()
+    server.login(sender_email, password)
+    return server
+
+
 def send_email(msg, send_to):
+    server = connect_to_smtp()
     try:
         sender_email = os.getenv('SENDER_EMAIL')
-        smtp_server = os.getenv('SMTP_SERVER')
-        port = os.getenv('SMTP_PORT')
-        password = os.getenv('SMTP_PASSWORD')
-
-        with smtplib.SMTP(smtp_server, port) as server:
-            server.starttls() 
-            server.login(sender_email, password)
-            text = msg.as_string()
-            server.sendmail(sender_email, send_to, text)
-            print("Email sent successfully!")
+        text = msg.as_string()
+        server.sendmail(sender_email, send_to, text)
+        print("Email sent successfully!")
     except Exception as e:
         print(f"Error: {e}")
+    finally:
+        server.quit()
 
-def create_email_message(send_to, sender_email, subject, body):
+def create_email_message(send_to, subject, body):
     msg = MIMEMultipart()
-    msg['From'] = sender_email
+    msg['From'] = os.getenv('SENDER_EMAIL')
     msg['To'] = send_to
     msg['Subject'] = subject
     msg.attach(MIMEText(body, 'plain'))
--- te.py
+++ te.py
@@ -1,33 +1,6 @@
-import asyncio
-import databases
-import orm
-
-database = databases.Database("sqlite:///db.sqlite")
-models = orm.ModelRegistry(database=database)
-
-
-class Note(orm.Model):
-    tablename = "notes"
-    registry = models
-    fields = {
-        "id": orm.Integer(primary_key=True),
-        "text": orm.String(max_length=100),
-        "completed": orm.Boolean(default=False),
-    }
-
-
-async def main():
-    # Create the database and tables
-    await models.create_all()
-
-    await Note.objects.create(text="Buy the groceries.", completed=False)
-
-    note = await Note.objects.get(id=1)
-    print(note)
-
+from app.feat.sendEmail import create_email_message,send_email,connect_to_smtp
 
 if __name__ == "__main__":
-    if asyncio.get_event_loop().is_running():
-        print("Event loop is already running. Cannot run asyncio.run()")
-    else:
-        asyncio.run(main())
+    send_to = 'zero99ck9@gmail.com'
+    formMail = create_email_message(send_to,'test send email','asdjakjhsdhashdsdf')
+    send_email(formMail,send_to)
