5262f48c4857d41a8ea70ddfe0f45703aebd0153
giauphan <giauphan012@gmail.com>
2024-03-11 09:35:52 +0700

    fix disconnect db

--- Model/User.py
+++ Model/User.py
@@ -1,9 +1,7 @@
 from migrations.create_table import User,database
 
 class UserModel(User):
-    database.connect()
     pass
-    database.disconnect
     async def CheckUser(username : str):
         user = await User.objects.filter(username=username).exists()
         return user
