e3e271e05e5033de22c54e0aa88836eecda2753d
giauphan <giauphan@users.noreply.github.com>
2024-03-18 04:12:53 +0000

    chore: fix code style

--- migrations/create_table.py
+++ migrations/create_table.py
@@ -81,7 +81,9 @@ class Income(orm.Model):
 
 
 async def add_account_balance_number_column():
-    await database.execute("ALTER TABLE Persons ALTER COLUMN account_balance SET DEFAULT 0")
+    await database.execute(
+        "ALTER TABLE Persons ALTER COLUMN account_balance SET DEFAULT 0"
+    )
 
 
 async def main():
